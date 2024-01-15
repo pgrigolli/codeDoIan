de9356f3684f5ec050b98d5f670b4afeffa6a441
pgrigolli <pedrogrigolli30@gmail.com>
2024-01-15 17:08:53 -0300

    First changes

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+struct Contato
+{
+    int id;
+    char nome[10];
+    char telefone[11];
+    char email[50];
+};
+
+typedef struct Contato contato;
+
+int addContato(contato c, contato* lista){
+
+
+/*
+TODO
+Adicionar o contato com id igual ao idx da lista
+*/
+
+}
+
+int removeContato(contato c, contato* lista){
+    
+}
+
+int main(){
+
+    contato listaDeContatos[20];
+
+
+}
+
+
